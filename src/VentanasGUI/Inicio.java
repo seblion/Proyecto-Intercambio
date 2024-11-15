30c840abd4b7c4523630e45eb2ea199936fb4847
domdume <domingacf@gmail.com>
2024-11-14 23:56:47 -0500

    Iniciar Sesion interfaz

---
+++ src/VentanasGUI/Inicio.java
@@ -0,0 +1,18 @@
+package VentanasGUI;
+
+import javax.swing.*;
+
+public class Inicio {
+    private JPanel inicio;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton registerButton;
+    private JLabel contraseña;
+    private JLabel usuario;
+    private JLabel iniciarSesionLabel;
+    private JLabel label1;
+
+    private void createUIComponents() {
+
+    }
+}
